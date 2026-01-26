170f31f0fd38bffd163e2635f6e665cb2374c695
Rohith2130 <rohithvarmadanthuluri9515@gmail.com>
2026-01-26 21:10:46 +0530

    Added frontend and database structure

---
+++ DB/myDy.sql
@@ -0,0 +1,23 @@
+create database myDiary;
+use myDiary;
+
+create table Users(
+ EmailID varchar(50) unique,
+ HashedPassword varchar(100),
+ ID int primary key auto_increment );
+ 
+ insert into Users(EmailID,HashedPassword)
+ values('rohith','sample');
+ 
+ select * from Users;
+ create table Posts(
+ ID int primary key auto_increment,
+ UserID int,
+ postTitle varchar(100),
+ postDescription varchar(1500),
+ foreign key (UserID) references Users(ID) );
+ 
+  select * from Posts;
+  
+ insert into Posts(UserID,postTitle,postDescription)
+ values(1,"Day at clg","it was good");
